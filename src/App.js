6bb6bd999d78b3d026d421288abb1468e20ae975
jvirma <jorbeful@gmail.com>
2020-07-06 23:01:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route} from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import logo from './logo.png';
+import Home from './components';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CssBaseline/>
+      <Route exact path={"/"} component={Home}/>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./Header";
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default Home
